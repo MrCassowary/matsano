5206db3a3ff65818d741ed24bedd976eb378cdce
Cassidy <mc.mathematics@gmail.com>
2016-10-21 13:04:24 +1100

    First commit

---
+++ bin/set1.jl
@@ -0,0 +1,21 @@
+module challenge1
+  using converters
+  if converters.hexto64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")== "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+    print("PASS")
+  else
+    print("fail")
+  end
+end
+
+
+module challenge2
+  using CryptoTools
+  using converters
+  x = "1c0111001f010100061a024b53535009181c"
+  y = "686974207468652062756c6c277320657965"
+  if  CryptoTools.hex_xor(x,y) == "746865206b696420646f6e277420706c6179"
+    print("PASS")
+  else
+    print("FAIL")
+  end
+end
---
+++ lib/converters.jl
@@ -0,0 +1,49 @@
+module converters
+
+  function hex_parse(s::String)
+    if ismatch(r"/[^0-9A-Fa-f]/",s)
+      throw("input is not a hex string!")
+    else
+      p = [parse(Int, s[i], 16) for i in collect(1:(length(s)))]
+      return p
+    end
+  end
+
+  function hex_to_bytes(s)
+    if ismatch(r"/[^0-9A-Fa-f]/",s)
+      throw("input is not a hex string!")
+    else
+        p = [parse(Int, s[i:i+1], 16) for i in collect(1:2:(length(s)-1))]
+      return p
+    end
+  end
+
+  function hexto64(t)
+    value = split(t,"")
+    l = [parse(Int,value[i],16) for i in collect(1:length(value))]
+    l = map(x->(bin(x,4)), l)
+    l =join(l,"")
+
+    if length(l)/8 % 3 !=0
+      padl = 3- length(l)/8 % 3
+      padl = Int(padl*8)
+      l = join(l*repeat("0",padl),"")
+    end
+
+    l = [l[i:i+5] for i in collect(1:6:length(l)-5)]
+    l=  map(x->parse(Int,x,2),l)
+    digsyms = [collect('A':'Z') ; collect('a':'z') ; '0':'9'; '+';'/';'=']
+  #to encode the padding
+    for c = 1:length(l)
+      if l[c] == 0
+        l[c] = 64
+      end
+    end
+
+    l=[l[i] = digsyms[l[i]+1] for i = 1:length(l)]
+    l=join(l,"")
+
+    return l
+  end
+
+end
