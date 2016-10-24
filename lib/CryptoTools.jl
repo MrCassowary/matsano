4ac1d64950331ada2f92e3b205fd6109003833e6
Cassidy <mc.mathematics@gmail.com>
2016-10-24 17:44:34 +1100

    derp

--- lib/CryptoTools.jl
+++ lib/CryptoTools.jl
@@ -1,7 +1,7 @@
 module CryptoTools
-using converters
-using DataStructures
-function hex_xor(a,b)
+  using converters
+  using DataStructures
+  function hex_xor(a,b)
     c = converters.hex_parse(a) $ converters.hex_parse(b)
     c = join(map(hex, c),"")
     return c
@@ -10,10 +10,10 @@ function hex_xor(a,b)
     if length(obs) == length(expt)
       chi2 = sum(((obs - expt).^2)./expt)
     else
-      throw("Test vectors are not of same length")
+      throw("Test vectors are not of same length $(length(obs)) and $(length(expt))" )
     end
     return chi2
-end
+  end
 
 #function scorestring(str)
     english_freq= Dict('a' => 0.0651738, 'b' => 0.0124248, 'c' => 0.0217339,
@@ -27,20 +27,20 @@ end
                     'y' => 0.0145984, 'z' => 0.0007836, ' ' => 0.1918182)
 
 #end
-function xor_key(string, key)
+  function xor_key(string, key)
   a = Int[]
   string = converters.hex_parse_bytes(string)
     for i = 1:length(string)
       push!(a,key $ string[i])
     end
     return a
-end
+  end
 
-function breakxor(string)
+  function breakxor(string)
   fkey = 'a'
   chi2min = 1e10
   fin = String[]
-   for key = 65:122
+   for key = 65:126 #66:122
     v = xor_key(string, key)
     #convert vector to ASCII representation
     v = map(Char, v)
@@ -48,9 +48,11 @@ function breakxor(string)
     n = length(v) #Determine length of input string
     t = n #duplicate length for loop
     #loop below scans backwards and deletes values from the string that aren't in our alphabet of interest
+    iskey = true
     while t > 0
       if !haskey(english_freq, v[t])
         deleteat!(v,t)
+        #iskey = false
       end
       t = t - 1
     end
@@ -60,7 +62,7 @@ function breakxor(string)
     #count the number of occurences of acceptable characters
     cdict = counter(v)
     #predclare observation vector and normalise the entries from the cdict
-    obsv = Float32[]
+    global obsv = Float32[]
     for i = 1:27
       if haskey(cdict, dind[i])
         push!(obsv, cdict[dind[i]]/n)
@@ -68,18 +70,20 @@ function breakxor(string)
         push!(obsv, 0.0)
       end
     end
-
-    if ChiSqTest(obsv, expv) < chi2min
-      print("potential better key found \n")
+    print(length(obsv))
+    if (ChiSqTest(obsv, expv) < chi2min) & iskey
+      #print("potential better key found \n")
       fkey = Char(key)
       chi2min = ChiSqTest(obsv, expv)
       fin = join(map(Char,xor_key(string, key)),"")
+      print("new oobs \n")
+      print(obsv)
+      print("\n")
       #convert vector to ASCII representation
     end
   end
-  #return fkey, fin
   return fkey, fin
-end
+  end
 breakxor("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
 end
 
